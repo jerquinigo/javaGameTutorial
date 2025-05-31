adeb86279c62a0fff65fd7a3b1c594239e563b4c
jerquinigo <jonathanerquinigo@pursuit.org>
2025-05-30 22:48:04 -0400

    finished first half of the lesson

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -15,7 +15,7 @@ public class Player extends Entity{
     public final int screenX;
     public final int screenY;
     //how many keys the player has
-    int hasKey = 0;
+    public int hasKey = 0;
 
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
@@ -135,13 +135,17 @@ public class Player extends Entity{
                     gp.playSE(1);
                     hasKey++;
                     gp.obj[i] = null;
-                    System.out.println("Key: " + hasKey);
+                    gp.ui.showMessage("You got a Key!");
                     break;
                 case "Door":
                     if(hasKey > 0) {
                         gp.playSE(3);
                         gp.obj[i] = null;
                         hasKey--;
+                        gp.ui.showMessage("You Opened the door");
+                    }
+                    else {
+                        gp.ui.showMessage("You need the key");
                     }
                     System.out.println("Key: " + hasKey);
                     break;
@@ -149,6 +153,12 @@ public class Player extends Entity{
                     gp.playSE(2);
                     speed += 3;
                     gp.obj[i] = null;
+                    gp.ui.showMessage("Speed Up");
+                    break;
+                case "Chest":
+                    gp.ui.gameFinished = true;
+                    gp.stopMusic();
+                    gp.playSE(4);
                     break;
             }
         }
